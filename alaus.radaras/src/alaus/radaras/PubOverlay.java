b70f8449b9324f2bd9a02f14c4e5b8c8f1144506
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 10:10:49 +0000

    Pub overlay class

---
+++ alaus.radaras/src/alaus/radaras/PubOverlay.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package alaus.radaras;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * @author Vincentas
+ * 
+ */
+public class PubOverlay extends ItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
+
+	private final Context context;
+
+	public PubOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		this.context = context;
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		overlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return overlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return overlays.size();
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = overlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
