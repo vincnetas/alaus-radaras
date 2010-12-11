aee23387931bceb1078dfdc66dae6d8c74a41645
gytis@fmu1.net <gytis@fmu1.net@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 18:19:25 +0000

    new

--- trunk/alaus.radaras/src/alaus/radaras/dao/BeerRadarDao.java
+++ trunk/alaus.radaras/src/alaus/radaras/dao/BeerRadarDao.java
@@ -152,9 +152,13 @@ public class BeerRadarDao {
 	
 	public Drawable getImage(String url) {
 		Resources resources = context.getResources();
-		Integer id = resources.getIdentifier("R.drawable.brand_"+url, null, null);
-		if(id == 0)
+		Integer id = 0;
+		if(url != null) {
+			id = resources.getIdentifier("R.drawable.brand_"+url, null, null);
+		}
+		if(id == 0) {
 			id = R.drawable.brand_default;
+		}
 			
 		return resources.getDrawable(id);
 	}
