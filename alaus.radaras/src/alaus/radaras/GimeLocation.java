8bf7321db7cc7857a92424394cb28f66f87291bd
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 16:04:00 +0000

    Working maps

--- alaus.radaras/src/alaus/radaras/GimeLocation.java
+++ alaus.radaras/src/alaus/radaras/GimeLocation.java
@@ -9,14 +9,8 @@ import java.util.Observer;
 
 import alaus.radaras.dao.BeerRadarDao;
 import alaus.radaras.dao.model.Pub;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
-import android.provider.Settings;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
@@ -41,8 +35,8 @@ public class GimeLocation extends MapActivity implements Observer {
 		super.onCreate(bunble);
 
 		setContentView(R.layout.map);		
-//		pubOverlay = new PubOverlay(getResources().getDrawable(R.drawable.icon), this);
-//		getMapView().getOverlays().add(pubOverlay);
+		pubOverlay = new PubOverlay(getResources().getDrawable(R.drawable.icon), this);
+		getMapView().getOverlays().add(pubOverlay);
 	}
 
 	private MapView getMapView() {
@@ -133,58 +127,5 @@ public class GimeLocation extends MapActivity implements Observer {
 	 */
 	public void setBeerRadarDao(BeerRadarDao beerRadarDao) {
 		this.beerRadarDao = beerRadarDao;
-	}
-	
-	private static final int ENABLE_GPS_DIALOG = 1;
-	
-	private static final int SEARCHING_LOCATION = 2;
-	
-	private static final int SHOW_GPS_SETTINGS = 120;
-
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onCreateDialog(int)
-	 */
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		Dialog result = null;
-		
-		switch (id) {
-			case ENABLE_GPS_DIALOG: {
-				result = new AlertDialog.Builder(this).
-					setTitle("Enable GPS").
-					setCancelable(false).
-					setMessage("This application requires GPS.").
-					setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int whichButton) {
-								Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-								startActivityForResult(intent, SHOW_GPS_SETTINGS);
-							}
-						}).create();
-				break;
-			}
-			case SEARCHING_LOCATION: {
-				result = getSearchLocationDialog();
-				break;
-			}		
-			default: {
-				break;
-			}
-		}
-
-        return result;
-	}
-	
-	private ProgressDialog searchLocationDialog = null;
-	
-	private Dialog getSearchLocationDialog() {
-		if (searchLocationDialog == null) {
-			searchLocationDialog = new ProgressDialog(this);
-			searchLocationDialog.setTitle("Searching...");
-			searchLocationDialog.setMessage("Aquiring GPS location...");
-			searchLocationDialog.setCancelable(false);
-			searchLocationDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		}
-		
-		return searchLocationDialog;
 	}
 }
