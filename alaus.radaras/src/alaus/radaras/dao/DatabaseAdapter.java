6c76a3dbb577c6abf2163aeea3dd8a26927913c5
Saulius.Maciulis@gmail.com <Saulius.Maciulis@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 13:40:05 +0000

    Added DB support

---
+++ alaus.radaras/src/alaus/radaras/dao/DatabaseAdapter.java
@@ -0,0 +1,71 @@
+package alaus.radaras.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseAdapter extends SQLiteOpenHelper {
+
+	private static final String LOG_TAG = "beer-radar-db";
+	
+	private static class Definition {
+		
+		public static String DB_NAME = "beer-radar";
+		
+		public static int DB_VERSION = 1;
+		
+		public static String PUBS = 
+	        "CREATE TABLE pubs(" +
+	        	"id 		TEXT PRIMARY KEY, " +
+	        	"title 		TEXT NOT NULL, " +
+	        	"longtitude REAL NOT NULL, " +
+	        	"latitude 	REAL NOT NULL, " +
+	        	"address 	TEXT, " +
+	        	"notes	 	TEXT, " +
+	        	"phone	 	TEXT, " +
+	        	"url	 	TEXT);";
+		
+		public static String BRANDS = 
+	        "CREATE TABLE brands(" +
+	        	"id 			TEXT PRIMARY KEY, " +
+	        	"title 			TEXT NOT NULL, " +
+	        	"icon			TEXT, " +
+	        	"description 	TEXT);";
+		
+		public static String PUBS_BRANDS = 
+	        "CREATE TABLE pubs_brands(" +
+	        	"pub_id			TEXT NOT NULL, " +
+	        	"beer_id 		TEXT NOT NULL);";
+
+	}
+	
+    public DatabaseAdapter(Context context) 
+    {
+        super(
+    		context, 
+    		Definition.DB_NAME, 
+    		null, 
+    		Definition.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) 
+    {
+        db.execSQL(
+        		Definition.PUBS + 
+        		Definition.BRANDS + 
+        		Definition.PUBS_BRANDS);
+    }
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(LOG_TAG, "Upgrading database from version " 
+        		+ oldVersion  + " to " + newVersion + 
+        		", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS pubs_brands;");
+        db.execSQL("DROP TABLE IF EXISTS pubs;");
+        db.execSQL("DROP TABLE IF EXISTS brands;");
+        onCreate(db);
+	}
+}
