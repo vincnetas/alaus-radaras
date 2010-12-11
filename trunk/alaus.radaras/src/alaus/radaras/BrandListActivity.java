545d691cc8fcc09d3e1a8038d2044ebc933fe90f
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 09:44:22 +0000

    Template activities

---
+++ trunk/alaus.radaras/src/alaus/radaras/BrandListActivity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package alaus.radaras;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * @author Vincentas
+ *
+ */
+public class BrandListActivity extends ListActivity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+	
+}
---
+++ trunk/alaus.radaras/src/alaus/radaras/PubActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package alaus.radaras;
+
+import android.app.Activity;
+
+/**
+ * @author Vincentas
+ *
+ */
+public class PubActivity extends Activity {
+
+}
