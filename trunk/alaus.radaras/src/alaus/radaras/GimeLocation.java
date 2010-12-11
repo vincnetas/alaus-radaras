c03cfb4091a046e5e5de51ba17851f2339b4839b
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-11 10:14:54 +0000

    Marker placement

--- trunk/alaus.radaras/src/alaus/radaras/GimeLocation.java
+++ trunk/alaus.radaras/src/alaus/radaras/GimeLocation.java
@@ -3,26 +3,44 @@
  */
 package alaus.radaras;
 
-import android.os.Bundle;
+import java.util.List;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 /**
  * @author Vincentas
- *
+ * based on : http://mobiforge.com/developing/story/using-google-maps-android
  */
 public class GimeLocation extends MapActivity {
 
-	
-	
 	/* (non-Javadoc)
 	 * @see com.google.android.maps.MapActivity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	protected void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
+	protected void onCreate(Bundle buicicle) {
+		super.onCreate(buicicle);
 		
 		setContentView(R.layout.map);
+		
+		MapView mapView = (MapView) findViewById(R.id.mapView);
+		
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+		PubOverlay overlay = new PubOverlay(drawable, this);
+		
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+		
+		overlay.addOverlay(overlayitem);
+		mapOverlays.add(overlay);
 	}
 
 	/* (non-Javadoc)
@@ -33,4 +51,12 @@ public class GimeLocation extends MapActivity {
 		return false;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onTouchEvent(android.view.MotionEvent)
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return super.onTouchEvent(event);
+	}
 }
