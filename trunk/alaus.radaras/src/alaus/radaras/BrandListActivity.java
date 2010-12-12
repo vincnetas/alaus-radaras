8ed1805c93854f1599172557a9b30e352dbe3056
gytis@fmu1.net <gytis@fmu1.net@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-12 09:35:03 +0000

    tab layout done

--- trunk/alaus.radaras/src/alaus/radaras/BrandListActivity.java
+++ trunk/alaus.radaras/src/alaus/radaras/BrandListActivity.java
@@ -3,58 +3,27 @@
  */
 package alaus.radaras;
 
-import java.util.List;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.view.View;
-import alaus.radaras.adapters.BrandListAdapter;
 import alaus.radaras.dao.BeerRadarDao;
-import alaus.radaras.dao.model.Brand;
+import android.os.Bundle;
 
 
 /**
  * @author LP
  *
  */
-public class BrandListActivity extends Activity {
+public class BrandListActivity extends BaseBrandListActivity {
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
-	private List<Brand> brands;
+
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		try {
-			setContentView(R.layout.list);
-        
-        	brands = BeerRadarDao.getInstance(this).getBrands();
-        }catch(Exception ex){
-			Toast.makeText(BrandListActivity.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-        ListView l1 = (ListView) findViewById(R.id.list);
-        l1.setAdapter(new BrandListAdapter(this, brands));
-
-        
-        l1.setOnItemClickListener(new ListView.OnItemClickListener(){
-
-        	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        		Brand br = brands.get(position);
-        		Intent inte = new Intent(BrandListActivity.this, GimeLocation.class);
-        		inte.putExtra(GimeLocation.BRAND_ID, br.getId());
-        		startActivity(inte);
-        	}
-            });
-
-        
+		setContentView(R.layout.list);
+		showBrands(BeerRadarDao.getInstance(this).getBrands(), R.id.list);
+  
 	}
+
+	
 
 	
 	
---
+++ trunk/alaus.radaras/src/alaus/radaras/CountryListActivity.java
@@ -0,0 +1,44 @@
+package alaus.radaras;
+
+import java.util.List;
+
+import alaus.radaras.adapters.CountryListAdapter;
+import alaus.radaras.adapters.TagListAdapter;
+import alaus.radaras.dao.BeerRadarDao;
+import alaus.radaras.dao.model.Country;
+import alaus.radaras.dao.model.Tag;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class CountryListActivity extends Activity {
+
+
+	private List<Country> countries;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+			setContentView(R.layout.country_list);
+        
+		countries = BeerRadarDao.getInstance(this).getCountries();
+        ListView l1 = (ListView) findViewById(R.id.countryList);
+        l1.setAdapter(new CountryListAdapter(this, countries));
+
+        
+        l1.setOnItemClickListener(new ListView.OnItemClickListener(){
+
+        	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        		Country country = countries.get(position);
+        		Intent inte = new Intent(CountryListActivity.this, CountryBrandsListActivity.class);
+        		inte.putExtra(CountryBrandsListActivity.COUNTRY, country.getCode());
+        		startActivity(inte);
+        	}
+            });
+
+	}
+	
+}
