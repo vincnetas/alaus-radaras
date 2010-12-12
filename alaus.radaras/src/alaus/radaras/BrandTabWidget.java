3ed71c971a699c158910e1986b671177117ea3e1
gytis@fmu1.net <gytis@fmu1.net@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-12 12:09:38 +0000

    initial tab colors

--- alaus.radaras/src/alaus/radaras/BrandTabWidget.java
+++ alaus.radaras/src/alaus/radaras/BrandTabWidget.java
@@ -3,8 +3,10 @@ package alaus.radaras;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 
 public class BrandTabWidget extends TabActivity {
 
@@ -13,29 +15,46 @@ public class BrandTabWidget extends TabActivity {
 	    setContentView(R.layout.brand_tabs);
 
 	    Resources res = getResources(); 
-	    TabHost tabHost = getTabHost(); 
+	    final TabHost tabHost = getTabHost(); 
 	    TabHost.TabSpec spec;
 	    Intent intent;  
 
 	    intent = new Intent().setClass(this, BrandListActivity.class);
 
 	    spec = tabHost.newTabSpec("alūs").setIndicator("Alūs",
-	                      res.getDrawable(R.drawable.bok_title))
+	                      res.getDrawable(R.drawable.ic_tab_brands))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    intent = new Intent().setClass(this, CountryListActivity.class);
 	    spec = tabHost.newTabSpec("šalys").setIndicator("Šalys",
-	                      res.getDrawable(R.drawable.bok_title))
+	                      res.getDrawable(R.drawable.ic_tab_countries))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    intent = new Intent().setClass(this, TagListActivity.class);
 	    spec = tabHost.newTabSpec("tipai").setIndicator("Tipai",
-	                      res.getDrawable(R.drawable.tag))
+	                      res.getDrawable(R.drawable.ic_tab_tags))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
 
 	    tabHost.setCurrentTab(0);
+	    //setTabColor(tabHost);
+	    tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+			
+			@Override
+			public void onTabChanged(String tabId) {
+				//setTabColor(tabHost);
+			}
+		});
+	    
+	}
+	
+	protected static void setTabColor(TabHost tabhost) {
+	    for(int i=0;i<tabhost.getTabWidget().getChildCount();i++)
+	    {
+	       tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#461B0A")); //unselected
+	    }
+	    tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#f9C22C")); // selected
 	}
 }
---
+++ alaus.radaras/src/alaus/radaras/viewholders/PubViewHolder.java
@@ -0,0 +1,11 @@
+package alaus.radaras.viewholders;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PubViewHolder {
+
+	public TextView title;
+	public ImageView img;
+
+}
