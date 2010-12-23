86eb99e066612ae46b2ea69c596a9b3ba7c2c311
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2010-12-23 13:35:06 +0000

    Using ListActivity

--- alaus.radaras/src/alaus/radaras/CountryListActivity.java
+++ alaus.radaras/src/alaus/radaras/CountryListActivity.java
@@ -5,38 +5,35 @@ import java.util.List;
 import alaus.radaras.adapters.CountryListAdapter;
 import alaus.radaras.service.BeerRadar;
 import alaus.radaras.service.model.Country;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class CountryListActivity extends Activity {
-
+public class CountryListActivity extends ListActivity {
 
 	private List<Country> countries;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-			setContentView(R.layout.country_list);
-        
+
 		countries = BeerRadar.getInstance(this).getCountries();
-        ListView l1 = (ListView) findViewById(R.id.countryList);
-        l1.setAdapter(new CountryListAdapter(this, countries));
 
-        
-        l1.setOnItemClickListener(new ListView.OnItemClickListener(){
+		getListView().setAdapter(new CountryListAdapter(this, countries));
+		getListView().setOnItemClickListener(new ListView.OnItemClickListener() {
 
-        	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        		Country country = countries.get(position);
-        		Intent inte = new Intent(CountryListActivity.this, CountryBrandsListActivity.class);
-        		inte.putExtra(CountryBrandsListActivity.COUNTRY, country.getCode());
-        		startActivity(inte);
-        	}
-            });
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+				Country country = countries.get(position);
+				Intent intent = new Intent(CountryListActivity.this, CountryBrandsListActivity.class);
+				intent.putExtra(CountryBrandsListActivity.COUNTRY, country.getCode());
+				startActivity(intent);
+			}
+		});
 
 	}
-	
+
 }
