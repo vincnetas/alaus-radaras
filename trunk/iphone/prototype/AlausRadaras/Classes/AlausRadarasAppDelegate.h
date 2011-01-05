a818daa66471bb401b38957bc0a7e11b954d7831
gytis@fmu1.net <gytis@fmu1.net@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2011-01-05 16:33:01 +0000

    Iphone prorotype test

---
+++ trunk/iphone/prototype/AlausRadaras/Classes/AlausRadarasAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AlausRadarasAppDelegate.h
+//  AlausRadaras
+//
+//  Created by Gytis on 1/5/11.
+//  Copyright Home 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface AlausRadarasAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ trunk/iphone/prototype/AlausRadaras/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AlausRadaras
+//
+//  Created by Gytis on 1/5/11.
+//  Copyright Home 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AlausRadarasAppDelegate");
+    [pool release];
+    return retVal;
+}
