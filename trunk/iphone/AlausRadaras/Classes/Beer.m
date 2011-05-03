1ea32983e5fe8fb54c6b763f1a735fe02eeba112
Laurynas.Remeika@gmail.com <Laurynas.Remeika@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2011-05-03 18:24:16 +0000

    Refactoring...

---
+++ trunk/iphone/AlausRadaras/Classes/Beer.m
@@ -0,0 +1,16 @@
+//
+//  Brand.m
+//  AlausRadaras
+//
+//  Created by Laurynas R on 1/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Beer.h"
+
+
+@implementation Beer
+
+@synthesize brandId, icon, label, pubsString;
+
+@end 
