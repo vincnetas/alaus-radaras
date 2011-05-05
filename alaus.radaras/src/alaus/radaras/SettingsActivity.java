e74c64e3818aa36419b87f7dd34f0b54414e073d
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2011-05-05 18:36:12 +0000

    Settings changed

--- alaus.radaras/src/alaus/radaras/SettingsActivity.java
+++ alaus.radaras/src/alaus/radaras/SettingsActivity.java
@@ -4,98 +4,55 @@ import alaus.radaras.settings.SettingsManager;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SettingsActivity extends Activity implements OnSeekBarChangeListener, OnCheckedChangeListener {
-	
-	SettingsManager settings;
-	TextView distanceTextView;
-	CheckBox distanceEnabledCheckBox;
-	SeekBar seekBar;
+public class SettingsActivity extends Activity {
 
-	  @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-	        setContentView(R.layout.settings);
-	        
-	        settings = new SettingsManager(this);
-	        distanceTextView = (TextView)findViewById(R.id.tvMaxDistance);
-	        distanceEnabledCheckBox = (CheckBox)findViewById(R.id.cbEnableDistance);
-	        
-	        int maxDistance = settings.getMaxDistance() ;
-	        seekBar = (SeekBar)findViewById(R.id.sbMaximumDistance);
-	        seekBar.setOnSeekBarChangeListener(this);
-	        
-	        if(maxDistance == SettingsManager.UNLIMITED_DISTANCE) {
-		        seekBar.setEnabled(false);
-		        distanceEnabledCheckBox.setChecked(false);
-	        } else {
-		        seekBar.setProgress(Math.max(maxDistance / SettingsManager.DISTANCE_STEP - 1, 0));
-		        distanceEnabledCheckBox.setChecked(true);
-	        }
-	        
-	        setDistanceText(maxDistance);
-	        
-	        CheckBox cbAskForLocation = (CheckBox)findViewById(R.id.cbAskForLocations);
-	        cbAskForLocation.setChecked(settings.askEnableLocationProvider());
-	        cbAskForLocation.setOnCheckedChangeListener(this);
-	        distanceEnabledCheckBox.setOnCheckedChangeListener(this);
-	        
-	  }
-	  
-	  private void setDistanceText(int maxDistance) {
-		  String distance;
-		  if(maxDistance == SettingsManager.UNLIMITED_DISTANCE) {
-			  distance = getString(R.string.settings_distance_disabled);
-		  } else if(maxDistance < 1000 ) {
-			  distance = getString(R.string.settings_distance_meters, maxDistance);
-		  } else {
-			  distance =  getString(R.string.settings_distance_km, ((double)maxDistance / 1000L));
-		  }
-		distanceTextView.setText(distance);
-		
-	}
-	  
-		@Override
-		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			if(buttonView.getId() == R.id.cbAskForLocations) {
-				settings.setAskOnNoLocation(isChecked);
-			} else {
-				seekBar.setEnabled(isChecked);
-				seekBar.setProgress(0);
-				
-				int distance = (isChecked) ? SettingsManager.DISTANCE_STEP : SettingsManager.UNLIMITED_DISTANCE;			
-				settings.setMaxDistance(distance);
-				setDistanceText(distance);
+	private SettingsManager settings;
 
-			}
-			
-		}
+	private static final int[] distances = new int[] { 500, 2000, 10000, SettingsManager.UNLIMITED_DISTANCE };
 
 	@Override
-		public void onStopTrackingTouch(SeekBar seekBar) {
-		
-		}
-		
-		@Override
-		public void onStartTrackingTouch(SeekBar seekBar) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-		}
-		
-		@Override
-		public void onProgressChanged(SeekBar seekBar, int progress,
-				boolean fromUser) {
+		settings = new SettingsManager(this);
 
-			int currentStep = progress +1;
-			settings.setMaxDistance(currentStep * SettingsManager.DISTANCE_STEP);
-			setDistanceText(currentStep * SettingsManager.DISTANCE_STEP);
-			
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		setContentView(R.layout.settings);
+
+		Spinner distanceSpinner = (Spinner) findViewById(R.id.settingsDistance);
+
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.settings_search_distance_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		distanceSpinner.setAdapter(adapter);
+		distanceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+				settings.setMaxDistance(distances[position]);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				settings.setMaxDistance(2000);
+			}
+		});
+
+		distanceSpinner.setSelection(getSelectedIndex());
+	}
+
+	private int getSelectedIndex() {
+		int distance = settings.getMaxDistance();
+		for (int i = 0; i < distances.length; i++) {
+			if (distances[i] == distance) {
+				return i;
+			}
 		}
 
+		return 1;
+	}
 }
