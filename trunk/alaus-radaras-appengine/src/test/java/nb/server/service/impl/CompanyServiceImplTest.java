225f4e5df32207c2a19226e8f4597ba2185365fb
vincnetas@gmail.com <vincnetas@gmail.com@ed55a4b0-3738-3b41-4b76-c30ff7ade623>
2011-08-10 20:02:38 +0000

    Dao tests

---
+++ trunk/alaus-radaras-appengine/src/test/java/nb/server/service/impl/CompanyServiceImplTest.java
@@ -0,0 +1,85 @@
+package nb.server.service.impl;
+
+import java.util.List;
+
+import nb.server.service.BeerService;
+import nb.server.service.CompanyService;
+import nb.server.service.UserService;
+import nb.shared.model.Beer;
+import nb.shared.model.Company;
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class CompanyServiceImplTest extends BaseServiceImplTest<Company>{
+
+	
+	public BeerService getBeerService() {
+		return guice.getInstance(BeerService.class);
+	}
+	
+	@Override
+	public CompanyService getBaseService() {
+		return guice.getInstance(CompanyService.class);
+	}
+
+	@Override
+	public Company getSample() {
+		Company result = new Company();
+		
+		return result;
+	}
+	
+	private Beer addBeer(String companyId, boolean approve) {
+		Beer beer = new Beer();
+		beer.setCompanyId(companyId);
+		
+		UserService userService = Mockito.mock(UserService.class);
+		if (approve) {
+			Mockito.when(userService.getCurrentUser()).thenReturn(getAdminUser());			
+		} else {
+			Mockito.when(userService.getCurrentUser()).thenReturn(getSimpleUser());
+		}
+		
+		BeerServiceImpl beerService = (BeerServiceImpl) getBeerService();
+		beerService.setUserService(userService);		
+		beerService.suggest(beer);
+	
+		return beer;
+	}
+	
+	@Test 
+	public void testGetSuggestedBeers() {
+		addBeer("1", false);		
+		addBeer("2", false);
+		addBeer("1", true);
+
+		List<Beer> suggestedBeers = getBaseService().getSuggestedBeers("1");
+		assertEquals(1, suggestedBeers.size());
+		
+		addBeer("1", false);
+		
+		suggestedBeers = getBaseService().getSuggestedBeers("1");
+		assertEquals(2, suggestedBeers.size());
+	}
+
+	@Test
+	public void testGetBeers() {
+		addBeer("1", false);
+		addBeer("2", false);
+		addBeer("1", true);
+
+		List<Beer> beers = getBaseService().getBeers("1");
+		assertEquals(1, beers.size());
+		
+		addBeer("1", true);
+
+		beers = getBaseService().getBeers("1");
+		assertEquals(2, beers.size());
+	}
+	
+	
+
+
+}
